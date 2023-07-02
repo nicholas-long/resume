483a9d11d5fea91f4a6a1b154d663e3790609f28
nicholas-long <nicholas.eden.long@gmail.com>
2023-07-02 03:29:13 -0500

    add resume-cli examples

---
+++ example.sh
@@ -0,0 +1,9 @@
+
+# first tried resumed
+#npm install -g resumed jsonresume-theme-even
+#resumed render --theme jsonresume-theme-even
+
+npm install -g resume-cli
+resume validate resume.json
+# uses resume.json as input file by default
+resume export resume.html
